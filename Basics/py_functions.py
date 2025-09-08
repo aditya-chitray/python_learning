e16ca54df61a0589eeee0c3920d7bb74a2f9d08d
aditya-chitray <chitray.16@gmail.com>
2025-09-08 23:49:16 +0530

    learning functions

---
+++ Basics/py_functions.py
@@ -0,0 +1,17 @@
+# program to learn about functions
+
+def sum(a,b):
+    return a+b
+
+def diff(a,b):
+    return b-a
+
+def even(a):
+    if a%2==0:
+        print("even")
+    else:
+        print("odd")
+
+print(sum(10,20))
+print(diff(10,20))
+even(50)
